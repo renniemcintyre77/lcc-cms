eb18cf783bf96cb5e9ec36c4eb08b5ea16e21714
Rennie McIntyre <rennie.mcintyre@sky.uk>
2021-10-28 00:00:06 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '53296f00ffafe29b816e653778b24d80'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '748e956f-b1e1-4bf1-a098-6c4a4b5ed7a2'
+};
