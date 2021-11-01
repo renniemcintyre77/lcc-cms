d1c0a430b0d81bf28764c0bc3e4ee04e1ef7ddfc
Rennie McIntyre <rennie.mcintyre@sky.uk>
2021-11-01 10:15:07 +0000

    latest changes

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'localhost'),
   port: env.int('PORT', 1337),
+  autoReload: { enabled: true },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '53296f00ffafe29b816e653778b24d80'),
